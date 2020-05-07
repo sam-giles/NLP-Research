9d10bfc8fa210966e13c97033a6ebd0c5541a55d
samgiles <samdotgiles@gmail.com>
2020-05-07 11:41:35 -0500

    Got results after preprocessing

--- DisambiguatedSim.py
+++ DisambiguatedSim.py
@@ -4,14 +4,23 @@ from gensim.models import Word2Vec
 import re
 import time
 from nltk.corpus import wordnet as wn
+import string
+from nltk.corpus import stopwords
+
+def preprocessCorpus(corpus):
+    #remove stopwords
+    stop_words = set(stopwords.words('english'))
+    corpus = [[word for word in sentence if word not in stop_words] for sentence in corpus]
+
+    #lowercase
+    corpus = [[word.lower() for word in sentence] for sentence in corpus]
+
+    #remove punctuation
+    corpus = [[word.translate(str.maketrans('', '', string.punctuation)) for word in sentence] for sentence in corpus]
+
+    return corpus
 
 start_time = time.time()
-
-t = wn.synsets('departure')[0]
-s = wn.synsets('chemistry')[0]
-print(t, s)
-print(t.shortest_path_distance(s))
-print(t.wup_similarity(s))
 
 #read in the sentences that we already disambiguated
 sentences_list = sentences.sentences
@@ -21,27 +30,41 @@ d_sentences_list = sentences.d_sentences
 sentences_list = [re.sub("[^\w]", " ",  sentence).split() for sentence in sentences_list]
 d_sentences_list = [re.sub("[^\w]", " ",  sentence).split() for sentence in d_sentences_list]
 
+#preprocess the sentences
+sentences_list = preprocessCorpus(sentences_list)
+d_sentences_list = preprocessCorpus(d_sentences_list)
+
 #use gutenberg corpus
 corpus = nltk.corpus.gutenberg.sents()
-#convert into type 'list'
-corpus = [item for item in corpus]
+#do preprocessing
+corpus = preprocessCorpus(corpus)
 d_corpus = corpus.copy()
+
 #only use the first 20% of each corpus for speed
-corpus = corpus[:int(len(corpus)/5)]
-d_corpus = d_corpus[:int(len(d_corpus)/5)]
+# corpus = corpus[:int(len(corpus)/5)]
+# d_corpus = d_corpus[:int(len(d_corpus)/5)]
+
 #add sentences
 corpus.extend(sentences_list)
 d_corpus.extend(d_sentences_list)
 
-# #the model takes a list of documents which are a list of sentences
+#the model takes a list of documents which are a list of sentences
+#train the model
 model = Word2Vec(corpus)
-# #d_model = model = Word2Vec(d_corpus_list, min_count=1,size= len(d_corpus_list),workers=3, window =3, sg = 1)
+d_model = Word2Vec(d_corpus)
 
-# for sentence in corpus:
-#     for word in sentence:
-#         try:
-#             print('departure', word, model.wv.similarity('departure', word))
-#         except:
-#             print("Word " + word + " not in vocabulary")
+d0 = wn.synsets('departure')[0]
+d1 = wn.synsets('departure')[1]
+d2 = wn.synsets('departure')[2]
+dl = [d0,d1,d2]
+wl = ['departure1', 'departure2', 'departure3']
+
+for i in range(len(dl)):
+    for j in range(len(wl)):
+        print(dl[i], dl[j], 'Shortest path: ', dl[i].shortest_path_distance(dl[j]), 'Word2Vec distance: ', d_model.wv.distance(wl[i], wl[j]))
+
+# for i in range(len(dl)):
+#     for j in range(len(wl)):
+#         print(dl[i], dl[j], 'Shortest path: ', dl[i].shortest_path_distance(dl[j]), 'Word2Vec distance: ', model.wv.distance(wl[i], wl[j]))
 
 print("Total execution time:", time.time()-start_time)
